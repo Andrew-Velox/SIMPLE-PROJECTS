2e23ca0be650400a3b67b64a787122ed813c6214
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2023-05-28 03:52:09 +0600

    Add files via upload

---
+++ PATTERN/pattern3.c
@@ -0,0 +1,32 @@
+"""
+  _________________Output___________
+  
+Enter: 5
+1
+22
+333
+4444
+55555
+
+"""
+
+#include <stdio.h>
+
+int main(){
+    int n,x,y,m=1;
+
+    printf("Enter: ");
+    scanf("%d",&n);
+
+    for(x=1;x<=n;x++){
+
+        for(y=1;y<=x;y++){
+            printf("%d",x);
+            
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
