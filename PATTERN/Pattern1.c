03a29fdb801e1e9b3dfb9d4e61d0f0272cd41273
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2023-05-28 03:26:23 +0600

    Create Pattern1.c

---
+++ PATTERN/Pattern1.c
@@ -0,0 +1,29 @@
+"""
+  _________________Output___________
+  
+Enter a Number:- 5
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+"""
+  
+#include <stdio.h>
+int main(){
+
+    int n,x,y;
+
+    printf("Enter a Number:- ");
+    scanf("%d",&n);
+
+    for (x=1;x<=n;x++){
+        for (y=1;y<=x;y++){
+            printf("%d ",y);
+         
+        }  
+        printf("\n");
+    }
+
+    return 0;
+}
