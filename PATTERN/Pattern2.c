10b44949da0a9395ffffa3bca31fdfca2a22be73
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2023-05-28 03:28:22 +0600

    Create Pattern2.c

---
+++ PATTERN/Pattern2.c
@@ -0,0 +1,29 @@
+"""
+  _________________Output___________
+  
+Enter: E
+A
+A B
+A B C
+A B C D
+A B C D E
+
+"""
+#include <stdio.h>
+int main(){
+
+    int n,x,y;
+
+    printf("Enter a Number:- ");
+    scanf("%d",&n);
+
+    for (x=1;x<=n;x++){
+        for (y=1;y<=x;y++){
+            printf("%d ",y);
+         
+        }  
+        printf("\n");
+    }
+
+    return 0;
+}
