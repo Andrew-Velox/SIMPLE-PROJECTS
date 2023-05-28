4b120445c02e881283da228627fef327da7a4dbe
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2023-05-28 17:46:10 +0600

    Create Pattern014.c

---
+++ PATTERN/Pattern014.c
@@ -0,0 +1,40 @@
+/*
+ _________________Output___________
+  
+Enter: 5
+1
+1 0
+1 0 1
+1 0 1 0
+1 0 1 0 1
+1 0 1 0
+1 0 1
+1 0
+1
+
+*/
+
+
+#include <stdio.h>
+
+int main(){
+
+    int n,x,y;
+
+    printf("Enter: ");
+    scanf("%d",&n);
+
+    for(x=1;x<=n;x++){
+        for(y=1;y<=x;y++){
+            printf("%d ",y%2);
+        }
+        printf("\n");
+    }
+    for(x=n-1;x>=1;x--){
+        for(y=1;y<=x;y++){
+            printf("%d ",y%2);
+        }
+        printf("\n");
+    }
+    return 0;
+}
