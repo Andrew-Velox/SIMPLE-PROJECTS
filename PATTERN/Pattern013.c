a4f570c130680f127006edbbb0523ab84faa1a36
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2023-05-28 17:41:54 +0600

    Create Pattern013.c

---
+++ PATTERN/Pattern013.c
@@ -0,0 +1,40 @@
+/*
+ _________________Output___________
+  
+Enter: E
+A
+A B
+A B C
+A B C D
+A B C D E
+A B C D
+A B C 
+A B
+A
+
+*/
+
+
+#include <stdio.h>
+
+int main(){
+
+    char n,x,y;
+
+    printf("Enter: ");
+    scanf("%c",&n);
+
+    for(x='A';x<=n;x++){
+        for(y='A';y<=x;y++){
+            printf("%c ",y);
+        }
+        printf("\n");
+    }
+    for(x=n-1;x>='A';x--){
+        for(y='A';y<=x;y++){
+            printf("%c ",y);
+        }
+        printf("\n");
+    }
+    return 0;
+}
