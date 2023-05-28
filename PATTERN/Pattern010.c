3497ac5d23b928afa59982fa8911a80f958fcfc2
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2023-05-28 17:02:00 +0600

    Create Pattern010.c

---
+++ PATTERN/Pattern010.c
@@ -0,0 +1,30 @@
+"""
+  _________________Output___________
+  
+Enter: G
+A B C D E F G
+A B C D E F
+A B C D E
+A B C D
+A B C
+A B
+A
+"""
+
+#include <stdio.h>
+
+int main(){
+
+    char n,x,y;
+
+    printf("Enter: ");
+    scanf("%c",&n);
+
+    for(x=n;x>='A';x--){
+        for(y='A';y<=x;y++){
+            printf("%c ",y);
+        }
+        printf("\n");
+    }
+    return 0;
+}
