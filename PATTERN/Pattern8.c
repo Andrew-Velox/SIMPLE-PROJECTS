d9f0ad65cfb315cf67b1c80af73afd848f774f45
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2023-05-28 16:54:07 +0600

    Create Pattern8.c

---
+++ PATTERN/Pattern8.c
@@ -0,0 +1,28 @@
+"""
+  _________________Output___________
+  
+Enter: 5
+5 5 5 5 5
+4 4 4 4
+3 3 3
+2 2
+1
+"""
+
+#include <stdio.h>
+
+int main(){
+
+    int n,x,y;
+
+    printf("Enter: ");
+    scanf("%d",&n);
+
+    for(x=n;x>=1;x--){
+        for(y=1;y<=x;y++){
+            printf("%d ",x);
+        }
+        printf("\n");
+    }
+    return 0;
+}
