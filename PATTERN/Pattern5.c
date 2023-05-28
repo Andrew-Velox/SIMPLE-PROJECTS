3ebe38d10c5a2d8833f89a91a5ef9781a9708cc3
Velox <99073463+Andrew-Velox@users.noreply.github.com>
2023-05-28 16:45:25 +0600

    Create Pattern5.c

---
+++ PATTERN/Pattern5.c
@@ -0,0 +1,28 @@
+"""
+  _________________Output___________
+  
+Enter: 5
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+"""
+  
+#include <stdio.h>
+
+int main(){
+
+    int n,x,y;
+
+    printf("Enter: ");
+    scanf("%d",&n);
+
+    for(x=n;x>=1;x--){
+        for(y=1;y<=x;y++){
+            printf("%d ",y);
+        }
+        printf("\n");
+    }
+    return 0;
+}
